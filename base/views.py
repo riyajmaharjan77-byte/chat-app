7116e8c89587934421300fd48d423db9546d61b0
riyaj maharjan <riyajmaharjan77@gmail.com>
2025-10-12 09:34:47 +0545

    Updated user table

--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Message
+from .models import Message, User
 # Create your views here.
 def home(request):
     if request.method== 'POST':
         user_message= request.POST.get('user_message')
         Message.objects.create(message=user_message)
-    return render(request,'index.html')
+    users = User.objects.all()
+    return render(request,'index.html', {'users':users})
