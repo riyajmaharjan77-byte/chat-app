919a4e46b3e3e7e0cb6f80318aeecb110bcfa557
riyaj maharjan <riyajmaharjan77@gmail.com>
2025-10-12 08:51:13 +0545

    Updated user table with gender icons

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Message
 # Create your views here.
 def home(request):
+    if request.method== 'POST':
+        user_message= request.POST.get('user_message')
+        Message.objects.create(message=user_message)
     return render(request,'index.html')
